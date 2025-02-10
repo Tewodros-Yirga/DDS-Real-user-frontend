aeb8a139b8daaafcaa321d4ade30834a5a1ef561
Tewodros Yirga <tewodrosy21@gmail.com>
2025-02-10 16:40:57 +0300

    Footer added

--- src/features/ContactUs.jsx
+++ src/features/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Section from "../design/Section";
+import Heading from "../design/Heading";
 
 const ContactUs = () => {
   const [formData, setFormData] = useState({
@@ -20,8 +21,8 @@ const ContactUs = () => {
 
   return (
     <Section>
-      <div className="container mx-auto w-[50%] bg-black p-4 text-white">
-        <h2 className="mb-4 text-2xl font-bold">Contact Us</h2>
+      <Heading className="heading" title="Contact us" />
+      <div className="container mx-auto w-[50%] rounded-xl bg-black p-4 text-white">
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label htmlFor="fullName" className="block text-sm font-medium">
@@ -33,7 +34,7 @@ const ContactUs = () => {
               name="fullName"
               value={formData.fullName}
               onChange={handleChange}
-              className="mt-1 block w-full rounded-md border-gray-300 bg-gray-800 px-1 text-white shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full rounded-md border-gray-300 bg-gray-800 px-1 py-2 text-white shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             />
           </div>
           <div>
@@ -46,7 +47,7 @@ const ContactUs = () => {
               name="email"
               value={formData.email}
               onChange={handleChange}
-              className="mt-1 block w-full rounded-md border-gray-300 bg-gray-800 px-1 text-white shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full rounded-md border-gray-300 bg-gray-800 px-1 py-2 text-white shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             />
           </div>
           <div>
